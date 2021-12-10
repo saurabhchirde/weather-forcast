91a8f68c970dd4edb17eb49b32ef34f9bc293de9
saurabhchirde <saurabh123.chirde@gmail.com>
2021-12-10 17:47:14 +0530

    feat: 3day forecast

--- script.js
+++ script.js
@@ -17,14 +17,24 @@ const footerEl = document.querySelector(".footer");
 const hrEl = document.querySelector(".hr");
 const iconEl = document.querySelector(".icon");
 
-// forecast el
-const forecastDate = document.querySelector(".forecastDate");
-const forecastLocation = document.querySelector(".forecastLocation");
-const forecastTemperature = document.querySelector(".forecastTemperature");
-const forecastWind = document.querySelector(".forecastWind");
-const forecastHumidity = document.querySelector(".forecastHumidity");
-const forecastOnlyTime = document.querySelector(".forecastOnlyTime");
-const forecastIcon = document.querySelector(".forecastIcon");
+// forecast el day1
+const forecastDate1 = document.querySelector(".forecastDate1");
+const forecastLocation1 = document.querySelector(".forecastLocation1");
+const forecastTemperature1 = document.querySelector(".forecastTemperature1");
+const forecastOnlyTime1 = document.querySelector(".forecastOnlyTime1");
+const forecastIcon1 = document.querySelector(".forecastIcon1");
+// forecast el day2
+const forecastDate2 = document.querySelector(".forecastDate2");
+const forecastLocation2 = document.querySelector(".forecastLocation2");
+const forecastTemperature2 = document.querySelector(".forecastTemperature2");
+const forecastOnlyTime2 = document.querySelector(".forecastOnlyTime2");
+const forecastIcon2 = document.querySelector(".forecastIcon2");
+// forecast el day3
+const forecastDate3 = document.querySelector(".forecastDate3");
+const forecastLocation3 = document.querySelector(".forecastLocation3");
+const forecastTemperature3 = document.querySelector(".forecastTemperature3");
+const forecastOnlyTime3 = document.querySelector(".forecastOnlyTime3");
+const forecastIcon3 = document.querySelector(".forecastIcon3");
 
 var iconTime = "day";
 
@@ -32,6 +42,9 @@ overall.classList.remove("bglight");
 locationEl.classList.remove("bglight");
 hrEl.classList.remove("hrVisible");
 
+document.querySelector(".day1").classList.remove("bglight");
+document.querySelector(".day2").classList.remove("bglight");
+document.querySelector(".day3").classList.remove("bglight");
 // weather api
 const apiUrl =
   "https://api.weatherapi.com/v1/current.json?key=dbb97da661744d27b6b80803210312&q=";
@@ -40,46 +53,62 @@ const apiUrl =
 const imageApi = "https://api.unsplash.com/search/photos?query=";
 
 //7 Day forcast api
-// const forecastApi =
-//   "http://api.weatherapi.com/v1/forecast.json?key=dbb97da661744d27b6b80803210312&q=";
+const forecastApi =
+  "http://api.weatherapi.com/v1/forecast.json?key=dbb97da661744d27b6b80803210312&q=";
 
-// function fetchForecastApi(fullForecastApi) {
-//   fetch(fullForecastApi)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       overall.innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
-//       forecastDate.innerText = data.forecast.forecastday[0].date;
-//       forecastIcon.innerHTML =
-//         'Current <img src="https:' + data.current.condition.icon + '"/></img>';
-//       forecastTemperature.innerHTML =
-//         "Max: " +
-//         data.forecast.forecastday[0].day.maxtemp_c +
-//         "˚C, Min :" +
-//         data.forecast.forecastday[0].day.mintemp_c +
-//         '˚C, <img src="https:' +
-//         data.forecast.forecastday[0].day.icon +
-//         '"/></img>';
+function fetchForecastApi(fullForecastApi) {
+  fetch(fullForecastApi)
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector(
+        ".forecastLocation"
+      ).innerText = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+      // day1
+      document.querySelector(".day1").classList.add("bglight");
+      forecastDate1.innerText = data.forecast.forecastday[0].date;
+      forecastIcon1.innerHTML =
+        '<img src="https:' +
+        data.forecast.forecastday[0].day.condition.icon +
+        '"/></img>';
+      forecastTemperature1.innerHTML =
+        "Max: " +
+        data.forecast.forecastday[0].day.maxtemp_c +
+        "˚C, Min :" +
+        data.forecast.forecastday[0].day.mintemp_c +
+        "˚C";
 
-//       // forecastOnlyTime
+      // day2
+      document.querySelector(".day2").classList.add("bglight");
+      forecastDate2.innerText = data.forecast.forecastday[1].date;
+      forecastIcon2.innerHTML =
+        '<img src="https:' +
+        data.forecast.forecastday[1].day.condition.icon +
+        '"/></img>';
+      forecastTemperature2.innerHTML =
+        "Max: " +
+        data.forecast.forecastday[1].day.maxtemp_c +
+        "˚C, Min :" +
+        data.forecast.forecastday[1].day.mintemp_c +
+        "˚C";
 
-//       // timeEl.innerText =
+      // day3
+      document.querySelector(".day3").classList.add("bglight");
+      forecastDate3.innerText = data.forecast.forecastday[2].date;
+      forecastIcon3.innerHTML =
+        '<img src="https:' +
+        data.forecast.forecastday[2].day.condition.icon +
+        '"/></img>';
+      forecastTemperature3.innerHTML =
+        "Max: " +
+        data.forecast.forecastday[2].day.maxtemp_c +
+        "˚C, Min :" +
+        data.forecast.forecastday[2].day.mintemp_c +
+        "˚C";
 
-//       // // hrEl.classList.add("hrVisible");
-//       // overall.innerText = `" ${partial} "`;
-//       // // overall.classList.add("bglight");
-//       // locationEl.innerText = `🌎 ${city}, ${region}, ${country}`;
-//       // locationEl.classList.add("bglight");
-
-//       // latitudeLongitudeEl.innerText = `Latitude: ${lat} \nLongitude: ${long}`;
-//       // temperatureEL.innerText = `🌡 Current Temp: ${temp}˚C \nFeels like: ${feelsLike}˚C`;
-//       // windEl.innerText = `Wind: ${wind_dir}, ${wind} km/h 🍃 `;
-//       // humidityEl.innerText = `Humidity: ${humid}%`;
-//       // dateEl.innerText = `Date : ${oDate}`;
-//       // timeEl.innerText = `Time: ${oTime}\nZone: ${tz}`;
-//       // footerEl.innerText = "with ❤️ by Saurabh Chirde";
-//     })
-//     .catch(() => (output.innerText = "Enter a valid city name ! "));
-// }
+      footerEl.innerText = "with ❤️ by Saurabh Chirde";
+    })
+    .catch(() => (output.innerText = "Enter a valid city name ! "));
+}
 
 function fetchApi(fullApiUrl) {
   fetch(fullApiUrl)
@@ -154,39 +183,33 @@ function fetchApi(fullApiUrl) {
 
 function findWeather() {
   let txt = inputCity.value;
-  if (txt !== "") {
-    const fullApiUrl = apiUrl + txt + "&aqi=yes";
+  output.classList.remove("hide");
+  document.querySelector(".outputForecast").classList.add("hide");
 
+  if (txt !== "") {
+    document.querySelector(".error").innerText = "";
+    const fullApiUrl = apiUrl + txt + "&aqi=yes";
     fetchApi(fullApiUrl);
   } else {
-    output.innerText = "Enter a city name! ";
+    document.querySelector(".error").innerText = "Enter a city name! ";
+    output.classList.add("hide");
   }
 }
+
 btnCheck.addEventListener("click", findWeather);
 
-// function weatherForecastSevenDays() {
-//   let txt = inputCity.value;
+function weatherForecastSevenDays() {
+  let txt = inputCity.value;
+  document.querySelector(".outputForecast").classList.remove("hide");
+  output.classList.add("hide");
 
-//   // erasing output div
-//   hrEl.classList.remove("hrVisible");
-//   overall.innerText = "";
-//   overall.classList.remove("bglight");
-//   locationEl.innerText = "";
-//   locationEl.classList.remove("bglight");
-//   iconEl.innerHTML = "";
-//   latitudeLongitudeEl.innerText = "";
-//   temperatureEL.innerText = "";
-//   windEl.innerText = "";
-//   humidityEl.innerText = "";
-//   dateEl.innerText = "";
-//   timeEl.innerText = "";
-
-//   if (txt !== "") {
-//     const fullForecastApi = forecastApi + txt + "&days=7";
-
-//     fetchForecastApi(fullForecastApi);
-//   } else {
-//     output.innerText = "Enter a city name! ";
-//   }
-// }
-// btnForcast.addEventListener("click", weatherForecastSevenDays);
+  if (txt !== "") {
+    document.querySelector(".error").innerText = "";
+    const fullForecastApi = forecastApi + txt + "&days=7";
+    fetchForecastApi(fullForecastApi);
+  } else {
+    document.querySelector(".error").innerText = "Enter a city name! ";
+    document.querySelector(".outputForecast").classList.add("hide");
+  }
+}
+btnForcast.addEventListener("click", weatherForecastSevenDays);
